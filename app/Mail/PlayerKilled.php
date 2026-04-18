50a82c5ca5ff7a6e5a2e271f4237317593fa125f
Evan Kim <evanhkim09@gmail.com>
2026-04-17 22:32:07 -0400

    Fix time zone

--- app/Mail/PlayerKilled.php
+++ app/Mail/PlayerKilled.php
@@ -4,20 +4,16 @@ namespace App\Mail;
 
 use App\Models\Kill;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PlayerKilled extends Mailable implements ShouldQueue
+class PlayerKilled extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public function __construct(public Kill $kill)
-    {
-        $this->afterCommit();
-    }
+    public function __construct(public Kill $kill) {}
 
     public function envelope(): Envelope
     {
@@ -33,13 +29,14 @@ class PlayerKilled extends Mailable implements ShouldQueue
             'victim:id,name',
         ]);
 
+        // All players from NCSSM so we can just lock the time zone.
         return new Content(
             markdown: 'mail.player-killed',
             with: [
                 'killerName' => $this->kill->killer->name,
                 'victimName' => $this->kill->victim->name,
-                'submittedAtFormatted' => $this->kill->created_at->timezone(config('app.timezone'))->format('M j, Y \\a\\t g:i A T'),
-                'expiresAtFormatted' => $this->kill->expires_at?->timezone(config('app.timezone'))->format('M j, Y \\a\\t g:i A T'),
+                'submittedAtFormatted' => $this->kill->created_at->timezone('America/New_York')->format('M j, Y \\a\\t g:i A T'),
+                'expiresAtFormatted' => $this->kill->expires_at?->timezone('America/New_York')->format('M j, Y \\a\\t g:i A T'),
                 'reviewUrl' => route('targets'),
             ],
         );

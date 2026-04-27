14027183b9b2120272ce44e312c6c54a146b6ebe
Evan Kim <evanhkim09@gmail.com>
2026-04-27 13:01:30 -0400

    Finish validation logic

--- config/game.php
+++ config/game.php
@@ -2,6 +2,9 @@
 
 return [
     'admin_emails' => array_filter(explode(',', env('ADMIN_EMAILS', ''))),
-    'enforce_signup_restrictions' => env('GAME_ENFORCE_SIGNUP_RESTRICTIONS', app()->environment(['production', 'testing'])),
+    'enforce_signup_restrictions' => env(
+        'GAME_ENFORCE_SIGNUP_RESTRICTIONS',
+        in_array(env('APP_ENV', 'production'), ['production', 'testing']),
+    ),
     'start' => env('GAME_START', '2026-04-13T00:00:00'),
 ];
